d21e4771d47a18461dc2a42bb72cb86112b89db5
Rina Tokhi <Rina.Tokhi7@gmail.com>
2021-06-24 17:46:33 +0430

    recipes cpmponent added

--- Recipe-book/src/app/app.module.ts
+++ Recipe-book/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { RecipesComponent } from './recipes/recipes.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    RecipesComponent
   ],
   imports: [
     BrowserModule
