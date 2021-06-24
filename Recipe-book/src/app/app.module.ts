5cb66f30e3a92aa9ce39848b87e7c32dbb55211b
Rina Tokhi <Rina.Tokhi7@gmail.com>
2021-06-24 17:48:06 +0430

    recipe-list added

--- Recipe-book/src/app/app.module.ts
+++ Recipe-book/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { RecipesComponent } from './recipes/recipes.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    RecipesComponent
+    RecipesComponent,
+    RecipeListComponent
   ],
   imports: [
     BrowserModule
