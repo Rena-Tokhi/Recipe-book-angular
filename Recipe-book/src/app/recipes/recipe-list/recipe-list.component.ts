499cb574e370f089fb9363bc7dd7b51a8ff7834e
Rina Tokhi <Rina.Tokhi7@gmail.com>
2021-06-26 16:14:27 +0430

    adding content to the recipe component

--- Recipe-book/src/app/recipes/recipe-list/recipe-list.component.ts
+++ Recipe-book/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { recipes } from '../recipes.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+
+  recipes : recipes [] = [
+    new recipes ('A test recipe', 'this is simply a test','https://www.gimmesomeoven.com/wp-content/uploads/2014/03/Cajun-Jambalaya-Recipe-with-Andouille-Sausage-Shrimp-and-Chicken-32.jpg')
+  ];
 
   constructor() { }
 
